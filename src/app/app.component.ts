12fc87b7a2762c63ced705c56b2cbb4ed3a44032
JerryngtonD <jerryngtond@gmail.com>
2017-11-03 15:25:21 +0300

    sort rendering was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,14 @@ export class AppComponent {
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-  console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-  return false; }
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
